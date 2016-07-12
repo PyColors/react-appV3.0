59f45b433dbb8f5d40c354e78ea3b6fd29da1b29
PyColors <patweb44@gmail.com>
2016-07-12 23:53:50 +0100

    add H2 component + test and box three

---
+++ app/components/H2/index.js
@@ -0,0 +1,16 @@
+/**
+*
+* H2
+*
+*/
+
+import React from 'react';
+
+
+function H2() {
+  return (
+    <h2 className={styles.heading2} {...props} />
+  );
+}
+
+export default H2;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,7 +11,8 @@
 
 import React from 'react';
 
-import H2 from 'components/H3';
+import H2 from 'components/H2';
+import H3 from 'components/H3';
 
 import styles from './styles.css';
 
@@ -25,6 +26,12 @@ export default class HomePage extends React.Component { // eslint-disable-line r
       <section className={styles.second}>
         <h3>Simple ! Sans stress. Sans pub.</h3>
         <h3>Nous gérons des playlists personnalisées basées sur votre personnalité</h3>
+      </section>
+
+      <section className={styles.three}>
+      <div className={styles.mask}>
+        <h2>Offres</h2>
+      </div>
       </section>
 
       </div>
